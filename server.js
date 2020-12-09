4692345b6455133c6e77e250580f70a822d6dae8
Deepak Bhamare <75732626+Deepakbhamare561@users.noreply.github.com>
2020-12-10 00:23:33 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+nano var http=require('http');
+var fs=require('fs');
+var url=require('url');
+var onCreateServer=function(req,res){
+var pathname=url.parse(req.url).pathname;
+fs.readFile(pathname.substr(1),function(err,data){
+res.writeHead(200,{'Content-Type':'text/html'});
+res.write(data.toString());
+res.end();
+});
+};
+var server=http.createServer(onCreateServer);
+server.listen(8081);
+console.log("Server running at http://127.0.0.1:8081");
